8fb0dfebb838cde360a7e5004636bd83231afd3a
alanding <dw395577197@163.com>
2019-12-15 14:13:05 +0800

    update bigdata_conf

--- linux/work-tools/database/mysql/mysql语句-demo.sql
+++
@@ -1,6 +0,0 @@
-
---  update user set authentication_string='' where user = 'root';
-
---  ALTER user 'root'@'localhost' IDENTIFIED BY 'root';
-
-GRANT all on *.* to 'alanding'@'%' IDENTIFIEd BY '193728456'
